99749b6726095921fff1e774d26f0362bd2cb561
Masao Kitamura <masaok@gmail.com>
2021-06-18 18:02:45 -0700

    Only the Stack Overflow answer works so far

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,32 +1,54 @@
-import * as React from 'react';
-import { StyleSheet } from 'react-native';
+import React, { useState, useEffect } from 'react'
 
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
+import { Text, View } from '../components/Themed'
 
+// https://www.digitalocean.com/community/tutorials/react-countdown-timer-react-hooks
 export default function TabOneScreen() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
-    </View>
-  );
-}
+  const calculateTimeLeft = () => {
+    let year = new Date().getFullYear()
+    const difference = +new Date(`${year}-10-1`) - +new Date()
+    let timeLeft = {}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-});
+    if (difference > 0) {
+      timeLeft = {
+        days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+        hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+        minutes: Math.floor((difference / 1000 / 60) % 60),
+        seconds: Math.floor((difference / 1000) % 60),
+      }
+    }
+
+    return timeLeft
+  }
+
+  const [timeLeft, setTimeLeft] = useState<any>(calculateTimeLeft())
+  const [year] = useState(new Date().getFullYear())
+
+  useEffect(() => {
+    setTimeout(() => {
+      setTimeLeft(calculateTimeLeft())
+    }, 1000)
+  })
+
+  const timerComponents: any = []
+
+  Object.keys(timeLeft).forEach((interval) => {
+    if (!timeLeft[interval]) {
+      return
+    }
+
+    timerComponents.push(
+      <span>
+        {timeLeft[interval]} {interval}{' '}
+      </span>
+    )
+  })
+
+  return (
+    <View>
+      <Text>HacktoberFest {year} Countdown</Text>
+      <Text>With React Hooks!</Text>
+      {timerComponents.length ? timerComponents : <Text>Time's up!</Text>}
+    </View>
+  )
+}
--- screens/TabTwoScreen.tsx
+++ screens/TabTwoScreen.tsx
@@ -1,17 +1,36 @@
-import * as React from 'react';
-import { StyleSheet } from 'react-native';
+import React, { useEffect, useState } from 'react'
+import { StyleSheet } from 'react-native'
 
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
+import { Text, View } from '../components/Themed'
+
+// This StackOverflow answer works:
+// https://stackoverflow.com/questions/57137094/implementing-a-countdown-timer-in-react-with-hooks
 
 export default function TabTwoScreen() {
+  // initialize timeLeft with the seconds prop
+  const [timeLeft, setTimeLeft] = useState(10)
+
+  useEffect(() => {
+    // exit early when we reach 0
+    if (!timeLeft) return
+
+    // save intervalId to clear the interval when the
+    // component re-renders
+    const intervalId = setInterval(() => {
+      setTimeLeft(timeLeft - 1)
+    }, 1000)
+
+    // clear interval on re-render to avoid memory leaks
+    return () => clearInterval(intervalId)
+    // add timeLeft as a dependency to re-rerun the effect
+    // when we update it
+  }, [timeLeft])
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab Two</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabTwoScreen.tsx" />
+    <View>
+      <Text>{timeLeft}</Text>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -29,4 +48,4 @@ const styles = StyleSheet.create({
     height: 1,
     width: '80%',
   },
-});
+})
